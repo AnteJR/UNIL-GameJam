ece3a822321cef0546c165d0a2aa1e01500f5ca1
Joël Rimaz <jo.ante2011@gmail.com>
2023-10-06 13:03:40 +0200

    Début barre de chargement

--- JS/main.js
+++ JS/main.js
@@ -3,6 +3,10 @@ let innerWidth = window.innerWidth;
 let innerHeight = window.innerHeight;
 let LANG = "ENG";
 
+let scale_incomplete = innerWidth/135;
+const proportion = Math.floor(scale_incomplete) + 1;
+console.log(proportion)
+
 kaboom({
     width: innerWidth,
     height: innerHeight,
