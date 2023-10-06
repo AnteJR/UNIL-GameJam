fad31caa01f7f3f487411034d9805ed651b4f0a2
Loïc Cattani <loic@cattani.co>
2023-10-06 18:05:29 +0200

    New bike roll

--- JS/onUpdate.js
+++ JS/onUpdate.js
@@ -34,6 +34,7 @@ onUpdate("background", (b) => {
 onUpdate("jaugeIn", (e) => {
     e.height = mapc(speed,minSpeed, maxSpeed, 5, 49)
     // mise à jour de la vitesse de l'animation selon la vitesse
-    player.animSpeed = mapc(speed,minSpeed, maxSpeed, 1, 2)
+    player.animSpeed = mapc(speed,minSpeed, maxSpeed, 1, 2);
+    // Mise à jour de la vitesse de playback du son selon la vitesse
     if (playerSound) playerSound.speed = mapc(speed, minSpeed, maxSpeed, 1, 1.4);
 });
