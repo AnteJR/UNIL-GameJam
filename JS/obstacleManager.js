f552d4a714913fec680c10573cfafbbd78e1ea5b
Sergenti <florian.rieder@unil.ch>
2023-10-06 18:13:15 +0200

    Select weighted random movement pattern

--- JS/obstacleManager.js
+++ JS/obstacleManager.js
@@ -1,10 +1,17 @@
 let localWindowTop;
 let localWindowBottom;
 
+// Usage example:
+const weightedMovementPatterns = [
+    { value: regularObstacle, weight: 3 },
+    { value: randomObstacle, weight: 2 },
+    { value: oneWayObstacle, weight: 1 },
+];
+
 let bottomMarker = add([
     rect(innerWidth, 1),
     area(),
-    pos(innerWidth/2, innerHeight + 200),
+    pos(innerWidth / 2, innerHeight + 200),
     anchor("center"),
     "bottomMarker"
 ]);
@@ -22,13 +29,32 @@ onUpdate(() => {
 });
 
 const spawnInterval = setInterval(() => {
+    const selectedMovementPattern = weightedRandom(weightedMovementPatterns).value;
+
     let e = background.add([
         rect(16, 16),
         outline(1),
         pos(0, localWindowTop - 40),
         area(),
         anchor("center"),
-        oneWayObstacle(),
+        selectedMovementPattern(),
         "obstacle"
     ]);
 }, 2000);
+
+function weightedRandom(items) {
+    const totalWeight = items.reduce((sum, item) => sum + item.weight, 0);
+    const randomValue = Math.random() * totalWeight;
+
+    let cumulativeWeight = 0;
+
+    for (const item of items) {
+        cumulativeWeight += item.weight;
+        if (randomValue <= cumulativeWeight) {
+            return item;
+        }
+    }
+
+    // This should not happen unless all weights are zero.
+    return null;
+}
