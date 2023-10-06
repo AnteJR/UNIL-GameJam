677d183adb68f4d088c367054abc24573809ad58
Isaac Pante <1324037+ipante@users.noreply.github.com>
2023-10-06 15:33:54 +0200

    Position du player

--- JS/player.js
+++ JS/player.js
@@ -1,7 +1,7 @@
 const player = add([
     sprite("player"),
     outline(1),
-    pos(innerWidth/2, innerHeight - 100),
+    pos(innerWidth/2, innerHeight - 270),
     area(),
     anchor("center"),
     scale(proportion)
