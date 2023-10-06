b3e31de05b972a88d71c53c0d2bc65108d532c43
Joël Rimaz <jo.ante2011@gmail.com>
2023-10-06 11:13:03 +0200

    Basic kaboom

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+// MAIN FILE - IMPORTING KABOOM, FILES, AND CONST
+let innerWidth = window.innerWidth;
+let innerHeight = window.innerHeight;
+let LANG = "ENG";
+
+kaboom({
+    width: innerWidth,
+    height: innerHeight,
+    clearColor: [0, 0, 0]
+});
