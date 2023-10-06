e5128992ed623095d1c20dde3ca1fed2faea745a
Sergenti <florian.rieder@unil.ch>
2023-10-06 13:09:04 +0200

    Added basic acceleration controls

--- JS/scrolling.js
+++ JS/scrolling.js
@@ -1,20 +1,55 @@
-let speed = 50;
+let speed = 10;
 let acceleration = 0;
+const accelerationRate = 1;
+const decelerationRate = 2;
+const minSpeed = 5;
+const maxSpeed = 20;
+let isMousePressed = false;
 
 loadSprite("terrain", "/assets/terrain.png");
 
 const background = add([
     sprite("terrain"),  // renders as a sprite
     pos(innerWidth/2, innerHeight),    // position in world
-    anchor("bot")
+    anchor("bot") // Set the anchor of the sprite on its bottom center
 ]);
 
 background.onUpdate(() => {
     if (background.pos.y >= background.height) {
+        background.pos.y = background.height;
         console.log("On est au bout !");
         return;
     }
+    if (isMousePressed) {
+        acceleration += accelerationRate;
+    } else {
+        acceleration -= decelerationRate;
+        if (acceleration < 0) {
+            acceleration = 0;
+        }
+    }
+    speed = speed + acceleration;
     let movement = vec2(0, speed);
     // .move() is provided by pos()
     background.move(movement);
+});
+
+onTouchStart(() => {
+    isMousePressed = true;
+    console.log("Touch start");
+});
+
+onTouchEnd(() => {
+    isMousePressed = false
+    console.log("Touch end");
+});
+
+onMousePress(() => {
+    isMousePressed = true;
+    console.log("Mouse down");
+});
+
+onMouseRelease(()=> {
+    isMousePressed = false;
+    console.log("Mouse up");
 });
