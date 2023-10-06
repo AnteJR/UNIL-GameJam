a9daaf60d091667164b7519e55cc4ddb734753b6
Sergenti <florian.rieder@unil.ch>
2023-10-06 16:27:00 +0200

    Random offset for obstacles movement

--- JS/obstacle.js
+++ JS/obstacle.js
@@ -1,31 +1,25 @@
 
-
-const enemy = background.add([
-    rect(16, 16),
-    outline(1),
-    pos(0, -200),
-    area(),
-    anchor("center"),
-    obstacle()
-]);
-
 function obstacle() {
-	// Can use local closed variables to store component state
+    // Can use local closed variables to store component state
     let speed = 1;
     let margin = 100;
+    const offset = Math.random() * 5
 
-	return {
+    return {
 
-		// The name of the component
-		id: "obstacle",
-		// If this component depend on any other components
-		require: [ "pos", "area"],
+        // The name of the component
+        id: "obstacle",
+        // If this component depend on any other components
+        require: ["pos", "area"],
 
-		// Runs every frame as long as the host object exists
-		update() {
-            let x = wave(margin / proportion, (innerWidth-margin) / proportion, time() * speed)
-            this.moveTo(x - (innerWidth/2)/proportion, this.pos.y);
-		}
-	}
-
+        // Runs every frame as long as the host object exists
+        update() {
+            let x = wave(margin / proportion, (innerWidth - margin) / proportion, (time() + offset) * speed)
+            this.moveTo(x - (innerWidth / 2) / proportion, this.pos.y);
+            if (this.pos.y < localWindowBottom) {
+                console.log("obstacle destroyed");
+                this.destroy();
+            }
+        }
+    }
 }
--- JS/obstacleManager.js
+++ JS/obstacleManager.js
@@ -1,20 +1,22 @@
-
-obstacles = [];
 let localWindowTop;
+let localWindowBottom;
 
 onUpdate(() => {
+    /* Compute the top and bottom of the screen in local coordinates */
     const currentScrollPosition = background.pos.y
 
     localWindowTop = -currentScrollPosition / proportion;
+    localWindowBottom = localWindowTop - innerHeight;
 });
 
 setInterval(() => {
-    background.add([
+    let e = background.add([
         rect(16, 16),
         outline(1),
-        pos(0, localWindowTop),
+        pos(0, localWindowTop - 40),
         area(),
         anchor("center"),
         obstacle()
     ]);
+
 }, 1000);
