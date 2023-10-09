1611af042d704ac63c1720ea8f171b2f6ff38462
Joël Rimaz <jo.ante2011@gmail.com>
2023-10-09 18:48:58 +0200

    Text styling

--- JS/homeScreen.js
+++ JS/homeScreen.js
@@ -28,22 +28,32 @@ scene("homeScreen", () => {
     ]);
 
     const txtFR = add([
-        text("Aide Sheepy à collecter les voeux de la communauté UNIL !", {
+        text("[wavy]Aide Sheepy à collecter les voeux de la communauté UNIL ![/wavy]", {
             font: "pixelFont",
             size: proportion < 6 ? 8 * proportion : (proportion < 15 ? 6 * proportion : 3 * proportion),
             width: proportion < 6 ? Math.floor((innerWidth / 5) * 4) : Math.floor(innerWidth / 2),
             align: "center",
+            styles: {
+                "wavy": (idx, ch) => ({
+                    pos: vec2(0, wave(-1, 1, time() + idx * 0.1)),
+                }),
+            },
         }),
         pos(innerWidth / 2, proportion < 6 ? Math.floor(innerHeight / 5 + innerHeight / 50) : Math.floor(innerHeight / 4 + innerHeight / 50)),
         anchor("top")
     ]);
 
     const txtENG = add([
-        text("Help Sheepy collect holiday greetings cards!", {
+        text("[wavy]Help Sheepy collect holiday greetings cards![/wavy]", {
             font: "pixelFont",
             size: proportion < 6 ? 8 * proportion : (proportion < 15 ? 6 * proportion : 3 * proportion),
             width: proportion < 6 ? Math.floor((innerWidth / 5) * 4) : Math.floor(innerWidth / 2),
             align: "center",
+            styles: {
+                "wavy": (idx, ch) => ({
+                    pos: vec2(0, wave(-1, 1, time() + idx * 0.1)),
+                }),
+            },
         }),
         pos(innerWidth / 2, proportion < 6 ? Math.floor((innerHeight / 10) * 7 + innerHeight / 50) : Math.floor((innerHeight / 10) * 7 + innerHeight / 50)),
         anchor("top")
