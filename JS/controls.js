dcafa1b6fc34ffd2e8de7831e406c3f355ba0881
Saara <astonedf@users.noreply.github.com>
2023-10-12 13:17:57 +0200

    instructions au début

--- JS/controls.js
+++ JS/controls.js
@@ -5,15 +5,29 @@ function addControls() {
         sprite(isPhone ? "phone_control" : "mouse_control", { anim: "tutorial" }),
         pos(Math.floor(innerWidth / 2), Math.floor(innerHeight / 2)),
         anchor("center"),
-        scale(proportion),
-        opacity(0),
-        { waitTime: 150 },
+        scale(proportion/2),
+        opacity(0.6),
+        //{ waitTime: 150 },
         "controls"
     ]);
 
-    onUpdate("controls", (e) => {
-        if(firstPress) destroy(e);
+    const controlTxt = add([
+        text(texte[LANG].control, {
+            font: "pixelFont",
+            align: "center",
+            size: proportion < 6 ? 8 * proportion : (proportion < 15 ? 6 * proportion : 3 * proportion),
+            width: proportion == 8 ? 130 * proportion : 130 * (proportion -1),
+        }),
+        color(0,0,0),
+        pos(Math.floor(innerWidth / 2), Math.floor(innerHeight / 50 * 3)),
+        anchor("top")
+    ]);
 
+    onUpdate("controls", (e) => {
+        if(firstPress) {
+            destroy(e);
+            controlTxt.destroy();
+        }
         if (e.waitTime > 0.18) e.waitTime -= dt()*60;
         else {
             if (e.opacity < 0.8) e.opacity += dt(); 
--- JS/main.js
+++ JS/main.js
@@ -58,12 +58,14 @@ const texte = {
         final: "Récolte terminée !",
         finalSmall: "L'Université de Lausanne vous souhaite de belles fêtes et une bonne année 2024 !",
         scoring: "Vous avez récupéré ",
-        objectif: " voeux de fin d'année !"
+        objectif: " voeux de fin d'année !",
+        control: "Laissez appuyé pour accélérer et relâchez pour ralentir"
     },
     ENG: {
         final: "Gathering completed!",
         finalSmall: "The University of Lausanne wishes you a wonderful holiday season and a happy new year!",
         scoring: "You managed to grab ",
-        objectif: " holiday greetings cards!"
+        objectif: " holiday greetings cards!",
+        control: "Press and hold to accelerate and release to slow down"
     }
 }
